ebbf6478b2850642b5de1dc88935bea75cbe1ec5
Stephen Sung <stephenrs7@gmail.com>
2017-12-01 03:19:46 -0800

    finalfinal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,14 +2,13 @@
 var wins = 0;
 var losses = 0;
 var current = 0;
+var goal = 0;
 
-$("#playerWins").text(wins);
-$("#playerLosses").text(losses);
+
 
 // Upon doc ready, show a randomized goal variable that the user must match (19-120)
 $(document).ready(function () {
-    var goal = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-    $("#goal").text(goal);
+    resetGame();
 });
 
 
@@ -18,21 +17,26 @@ function randNum() {
 }
 // Now for the hard part. Creating multiple crystals each with their own unique number value.
 function resetGame() {
-    var goal = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-    $("#goal").text(goal);
+    goal = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    current = 0;
+    
     // We begin by expanding our array to include four options.
     var crystalValue = ["#btn1", "#btn2", "#btn3", "#btn4"];
     for (var i = 0; i < crystalValue.length; i++) {
         var crystalId = crystalValue[i];
+        console.log(crystalId);
         $(crystalId).attr("data-crystalvalue", randNum());
-        crystalValue = parseInt(crystalValue);
-
-        console.log(crystalValue);
+        console.log($(crystalId).attr("data-crystalvalue"));
     }
+    $("#goal").text(goal);
+    $("#playerWins").text(wins);
+    $("#playerLosses").text(losses);
+    $("#current").text(current);
 
 }
 
 $(".crystal-img").on("click", function () {
+    console.log();
 
     // Determining the crystal's value requires us to extract the value from the data attribute.
     // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
@@ -40,32 +44,28 @@ $(".crystal-img").on("click", function () {
     // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
 
     var increment = ($(this).attr("data-crystalvalue"));
+    console.log($(this));
     increment = parseInt(increment);
     console.log(increment);
     // We then add the crystalValue to the user's "counter" which is a global variable.
     // Every click, from every crystal adds to the global counter.
     current += increment;
-    current = parseInt(current)
+    current = parseInt(current);
     $("#current").text(current);
     console.log(current);
 
     // All of the same game win-lose logic applies. So the rest remains unchanged.
-    alert("New score: " + current);
+
+    if (current === goal) {
+        wins++;
+        resetGame();
+
+    }
+
+    else if (current > goal) {
+        losses++;
+        resetGame();
+
+    }
+
 });
-if (current === goal) {
-    alert("You win!");
-    wins++;
-    resetGame();
-
-}
-
-else if (current >= goal) {
-    alert("You lose!!");
-    losses++;
-    resetGame();
-
-}
-
-
-
-
